20f32caf283442e0a1f0a55740902cc56e544264
José Naves <josenaves@gmail.com>
2018-08-08 08:13:14 -0300

    Track score and rounds

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +33,10 @@ class ViewController: UIViewController {
         let difference = abs(currentValue - targetValue)
         let points = 100 - difference
         
-        let message = "You scored \(points) points" 
+        score += points
+        round += 1
+
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
@@ -59,6 +66,8 @@ class ViewController: UIViewController {
     
     func updateLabels() {
         self.targetLabel.text = String(targetValue)
+        scoreLabel.text = "\(score)"
+        roundLabel.text = String(round)
     }
 }
 
