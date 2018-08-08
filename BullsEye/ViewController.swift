4fe3611c7d3a1c893044aa3b210365d62e869205
José Naves <josenaves@gmail.com>
2018-08-08 12:16:48 -0300

    Change title of alert and add bonus points

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -31,14 +31,29 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let difference = abs(currentValue - targetValue)
-        let points = 100 - difference
+        var points = 100 - difference
         
         score += points
         round += 1
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
 
         let message = "You scored \(points) points"
         
-        let alert = UIAlertController(title: "Hello, World",
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
         
@@ -47,7 +62,6 @@ class ViewController: UIViewController {
                                    handler: nil)
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
         
         startNewRound()
